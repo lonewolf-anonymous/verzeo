ad3e24199ade8e024069159ef317d60f822c9d27
Abhishek Gupta <abhishekgupta7184@gmail.com>
2021-06-13 16:15:38 +0530

    task 2 done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:verzeo/home.dart';
-import 'details.dart' as det;
 
 void main() {
   runApp(MyApp());
